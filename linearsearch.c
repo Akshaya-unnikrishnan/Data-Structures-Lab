8d847301ea85ad38fa105fd199abd57728507683
Akshaya Unnikrishnan <akshayaaunni@gmail.com>
2025-11-28 19:23:00 +0530

    Update linearsearch.c

--- linearsearch.c
+++ linearsearch.c
@@ -1,2 +1,28 @@
 //linear search
+#include <stdio.h>
+void main()
+{
+    int n,a[30],i,key,found=0;
+    printf("Enter the limit:");
+    scanf("%d",&n);
+    if(n>30)
+    printf("Too many elements");
+    else
+    {
+        printf("Enter the elements:");
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        printf("Enter the key(element to be searched:");
+        scanf("%d",&key);
+        for(i=0;i<n;i++)
+        if(a[i]==key)
+        {
+           printf("Found at position %d",i+1);
+            found=1;
+            break;
+    }
+    if (found==0)
+    printf("Not Found");
+}
+}
 
