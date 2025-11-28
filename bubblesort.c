c3686f9a818b525173b24c0881cdd8cd1d940ed6
Akshaya Unnikrishnan <akshayaaunni@gmail.com>
2025-11-28 19:22:35 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,1 +1,18 @@
-//BUbble sort
+//BUbble sort-sorting an array
+#include<stdio.h>
+void main() {
+int a[20],i,j,temp,n;
+printf("Enter the limit");
+scanf("%d",&n);
+printf("Enter the element");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++) {
+for(j=0;j<n-i-1;j++) {
+if(a[j]>a[j+1]) {
+temp=a[j];
+a[j]=a[j+1];
+a[j+1]=temp; }}}
+printf("The sorted array is ");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]); }
